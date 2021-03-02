6cce613b21885f9558ae8f722ac37f89daa9dcb3
babuece97 <babuece97@yahoo.com>
2021-03-02 02:16:58 +0100

    34.4

---
+++ src/components/Header/Hheader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../../images/logo.png';
+import './Header.css';
+
+const Headerr
+
+ = () => {
+    return (
+        <div className="headeer">
+            <img src= {logo} alt=""/>
+        </div>
+    );
+};
+
+export default Headerr
+;
