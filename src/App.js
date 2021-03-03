9bbcc33f658136470e73cb3ce5ced59c901591d6
babuece97 <babuece97@yahoo.com>
2021-03-03 04:02:49 +0100

    34.8v4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import গতকালR from './components/Header/গতকাল';
-import ProducttF from './components/FProduct/fProduct';
 import Shoppp from './components/Shop/Shop';
 
 
