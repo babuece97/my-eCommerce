5cbb368a762e8388d7c95478a751c4dccb4b103d
babuece97 <babuece97@yahoo.com>
2021-03-03 10:36:03 +0100

    34.8v5

--- src/components/FProduct/fProduct.js
+++ src/components/FProduct/fProduct.js
@@ -8,6 +8,9 @@ const fProducttt = (props) => {
             <h3>{props.item.name }</h3>
             <h6> Im jinisgggggggggg</h6>
             <h3>{props.item.key}</h3>
+            <h6> Im Mal2</h6>
+            <h3>{props.item.price}</h3>
+            <h3> Im Mal3</h3>
         </div>
     );
 };
