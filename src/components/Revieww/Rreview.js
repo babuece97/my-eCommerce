c0613beaa1b2c47ddeec2b1e9de0cf605f547fb9
babuece97 <babuece97@yahoo.com>
2021-03-10 01:52:31 +0100

    37-8

--- src/components/Revieww/Rreview.js
+++ src/components/Revieww/Rreview.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import fakeData from '../../fakeData';
+import { getDatabaseCart } from '../../utilities/databaseManager';
 
 const Rreview = () => {
+    const [cart, setCart] = useState([]);
+    useEffect(()=> {
+        // from cart
+        const savedCart = getDatabaseCart();
+        const productKeyss = Object.keys(savedCart); // Reserve Method keys
+        const cartProducts = 
+        productKeyss.map( keY => {
+            const productt = fakeData.find( prdss => prdss.key === keY);
+            productt.quantitYy = savedCart [keY];
+            return productt;
+        });
+        setCart(cartProducts);
+
+    }, [])
     return (
         <div>
             <h1> My name is REWWWiewwww</h1>
+            <h1> carttt  itemms :{cart.length}</h1>
         </div>
     );
 };
